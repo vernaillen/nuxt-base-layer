011602e9e39a06b6f5c36ad23b94f89b2d5be9d3
Wouter Vernaillen <wouter@vernaillen.com>
2023-04-20 09:01:58 +0200

    added tailwind & nuxt-icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,14 @@
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+const currentDir = dirname(fileURLToPath(import.meta.url))
+
 export default defineNuxtConfig({
+  modules: [
+    '@nuxtjs/tailwindcss',
+    'nuxt-icon'
+  ],
+  css: [
+    join(currentDir, './assets/css/tailwind.css'),
+    'animate.css/animate.min.css',
+  ],
 })
