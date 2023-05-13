5cf7cb533892cb059fa2e0db07d539efe6ae5aba
Wouter Vernaillen <wouter@vernaillen.com>
2023-05-13 18:24:11 +0200

    nuxt schema + package updates

---
+++ nuxt.schema.ts
@@ -0,0 +1,41 @@
+export default defineNuxtSchema({
+  appConfig: {
+    instagram: {
+      enabled: false
+    },
+    socials: {
+      twitter: {
+        url: '',
+        icon: 'simple-icons:twitter'
+      },
+      github: {
+        url: '',
+        icon: 'simple-icons:github'
+      },
+      facebook: {
+        url: '',
+        icon: 'simple-icons:facebook'
+      },
+      instagram: {
+        url: '',
+        icon: 'simple-icons:instagram'
+      },
+      youtube: {
+        url: '',
+        icon: 'simple-icons:youtube'
+      },
+      medium: {
+        url: '',
+        icon: 'simple-icons:medium'
+      },
+      mastodon: {
+        url: '',
+        icon: 'simple-icons:mastodon'
+      },
+      mastodon: {
+        url: '',
+        icon: 'simple-icons:whatsapp'
+      }
+    }
+  }
+})
