1f3fb10d81c1279d401ee7422670b3f5b2257024
Wouter Vernaillen <wouter@vernaillen.com>
2023-05-13 18:50:54 +0200

    add name to socials

--- nuxt.schema.ts
+++ nuxt.schema.ts
@@ -5,34 +5,42 @@ export default defineNuxtSchema({
     },
     socials: {
       twitter: {
+        name: 'Twitter',
         url: '',
         icon: 'simple-icons:twitter'
       },
       github: {
+        name: 'GitHub',
         url: '',
         icon: 'simple-icons:github'
       },
       facebook: {
+        name: 'Facebook',
         url: '',
         icon: 'simple-icons:facebook'
       },
       instagram: {
+        name: 'Instagram',
         url: '',
         icon: 'simple-icons:instagram'
       },
       youtube: {
+        name: 'YouTube',
         url: '',
         icon: 'simple-icons:youtube'
       },
       medium: {
+        name: 'Medium',
         url: '',
         icon: 'simple-icons:medium'
       },
       mastodon: {
+        name: 'Mastodon',
         url: '',
         icon: 'simple-icons:mastodon'
       },
-      mastodon: {
+      whatsapp: {
+        name: 'WhatsApp',
         url: '',
         icon: 'simple-icons:whatsapp'
       }
