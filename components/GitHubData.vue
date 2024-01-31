75ef997138c17d798a3394caf7a1cdff894b7c26
Wouter Vernaillen <wouter@vernaillen.com>
2024-01-31 07:01:56 +0100

    update footer with Nuxt info

--- components/GitHubData.vue
+++ components/GitHubData.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import type { CommitResponse } from '@/types/github'
 const appConfig = useAppConfig()
+const config = useRuntimeConfig()
 
 export interface Props {
   locale: string
@@ -14,10 +15,16 @@ const { data: commits } = useLazyAsyncData<CommitResponse[]>('commits', () =>
 </script>
 
 <template>
-  <span v-if="commits && commits[0]">
-    last updated on <NuxtTime :datetime="commits[0].commit.committer.date" month="long" day="numeric" year="numeric" :locale="locale" />
-    (last commit: <NuxtLink :href="commits[0].html_url" target="_blank">
-      {{ commits[0].sha.slice(0, 7) }}
-    </NuxtLink>)
+  <span v-if="commits && commits[0]" class="grid md:grid-cols-2">
+    <div class="md:text-end md:mr-3">
+      built with <Icon name="mdi:heart" color="red" />
+      using <NuxtLink href="https://nuxt.com" target="_blank"><Icon class="w-12 -mt-1" name="logos:nuxt" /> v{{ config.public.nuxtVersion }}</NuxtLink>
+    </div>
+    <div class="md:text-start md:ml-3">
+      last updated on <NuxtTime :datetime="commits[0].commit.committer.date" month="long" day="numeric" year="numeric" :locale="locale" /> -
+      <NuxtLink :href="commits[0].html_url" target="_blank">
+        <Icon name="mdi:github" /> {{ commits[0].sha.slice(0, 7) }}
+      </NuxtLink>
+    </div>
   </span>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,15 @@
+import pkg from './node_modules/nuxt/package.json'
+
 export default defineNuxtConfig({
   modules: [
     '@nuxt/image',
     '@vueuse/nuxt',
+    'nuxt-icon',
     'nuxt-time'
-  ]
+  ],
+  runtimeConfig: {
+    public: {
+      nuxtVersion: pkg.version
+    }
+  }
 })
