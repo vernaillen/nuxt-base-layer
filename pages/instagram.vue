96030678cd6e0a8508e0a7e7faa33fd5c8671ba7
Wouter Vernaillen <wouter@vernaillen.com>
2024-06-18 12:29:39 +0200

    fix instagram sbi script

--- pages/instagram.vue
+++ pages/instagram.vue
@@ -23,21 +23,25 @@ if (igConfig.enabled) {
       {
         src: 'https://code.jquery.com/jquery-3.6.4.min.js',
         integrity: 'sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=',
-        crossorigin: 'anonymous'
+        crossorigin: 'anonymous',
+        tagPosition: 'head',
       },
       {
         id: 'sbi_scripts-js-extra',
         innerHTML: '/* <![CDATA[ */ var sb_instagram_js_options = { "font_method": "svg", "resized_url": "' + igConfig.wpBaseUrl + 'wp-content/uploads/sb-instagram-feed-images/", "placeholder": "' + igConfig.wpBaseUrl + 'wp-content/plugins/instagram-feed-pro/img/placeholder.png", "br_adjust": "1" }; var sbiTranslations = { "share": "Share" }; /* ]]> */',
-        type: 'text/javascript'
+        type: 'text/javascript',
+        tagPosition: 'head',
+      },
+      {
+        src: '/sbi/iframeResizer.contentWindow.min.js',
+        type: 'text/javascript',
+        tagPosition: 'head',
       },
       {
         id: 'sbi_scripts-js',
         src: igConfig.wpBaseUrl + 'wp-content/plugins/instagram-feed-pro/js/sbi-scripts.min.js?ver=6.2.3',
-        type: 'text/javascript'
-      },
-      {
-        src: '/sbi/iframeResizer.contentWindow.min.js',
-        type: 'text/javascript'
+        type: 'text/javascript',
+        tagPosition: 'bodyClose',
       }
     ],
     style: [
@@ -58,10 +62,12 @@ onMounted(() => {
 </script>
 
 <template>
-  <!-- eslint-disable vue/no-v-html -->
-  <div
-    v-if="igContent"
-    class="m-auto h-[400px]"
-    v-html="igContent"
-  />
+  <div>
+    <!-- eslint-disable vue/no-v-html -->
+    <div
+      v-if="igContent"
+      class="m-auto h-[400px]"
+      v-html="igContent"
+    />
+  </div>
 </template>
